58cbc60b6efecc4f967daff4ba62c53895e9bc82
Brandon Arias Cjuno <brandon12arias@gmail.com>
2025-09-09 23:34:38 -0500

    cambios en el proyecto

--- students/forms.py
+++ students/forms.py
@@ -6,3 +6,6 @@ class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
         fields = ["first_name", "last_name", "email", "enrollment_date"]
+        widgets = {
+            'enrollment_date': forms.DateInput(attrs={'type': 'date'})
+        }
